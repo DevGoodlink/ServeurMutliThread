32f106c05d2b9917f522eb2e65a6b3e5bcff81fb
Black KraKen <contact@guillaumeperez.fr>
2018-03-19 15:14:30 +0100

    Documentation joueur et requete

--- Joueur.java
+++ Joueur.java
@@ -1,20 +1,45 @@
 import java.io.Serializable;
 class Joueur implements Serializable{
+
+    /** @type long numéro de serie définie pour le partage entre client et serveur */
     final static long serialVersionUID =1L;
+   
+    /** @type String Nom et prenom du joueur */
     String nom,prenom;
+
+    /** @type String license du joueur */
     int licence=0;
+
+    /** @type int Score du joueur */
     int score;
+
+    /** @type int nombre de partie joué */
     int nbrJeux=0;
+
+    /** @type boolean le joueur et connectée */
     boolean CONNECTED = false;
     
+    /**
+     * Constructeur de joueur
+     * @param  licence license du joueur
+     */
     public Joueur(int licence) {
         this.licence=licence;
     }
     
+    /**
+     * Constructeur par defautl du joueur
+     */
     public Joueur() {
         super();
     }
-   
+    
+    /**
+     * Cnostructeur complet du joueur
+     * @param  nom     Nom du joueur
+     * @param  prenom  Prenom du joueur
+     * @param  licence License du joueur
+     */
     public Joueur(String nom,String prenom,int licence){
         this.nom=nom;
         this.prenom=prenom;
@@ -25,12 +50,14 @@ class Joueur implements Serializable{
     public String toString() {
         return "nom : "+this.nom+" prenom "+this.prenom+" score = "+this.score +" nombre de jeux = "+nbrJeux;
     }
+   
     /**
      * @return the score
      */
     public int getScore() {
         return score;
     }
+   
     @Override
     public boolean equals(Object obj) {
         if(! (obj instanceof Joueur)) return false;
--- Requete.java
+++ Requete.java
@@ -2,25 +2,50 @@ import java.io.Serializable;
 
 
 class Requete implements Serializable{
+    
+    /** @type long numéro de serie de la classe */
     final static long serialVersionUID = 1L;
+
+    /** @type Joueur joueur à envoyé */
     Joueur j;
+
+    /** @type String intention de la requete */
     String intent;
+
+    /** @type String contenue texte de la requete */
     String answer;
+
+    /** @type liong temps de jeux */
     long time;
+
+    /** Constructeur de base */
     public Requete(){}
+
+    /**
+     * Constructeur complet
+     * @param  j      Joueur
+     * @param  intent intention
+     * @param  answer contenue texte
+     * @param  time   temps
+     */
     public Requete(Joueur j, String intent,String answer,long time){
         this.j=j;
         this.intent = intent;
         this.answer=answer;
         this.time=time;
     }
+
+    /**
+     * Getter du joueur
+     * @return le joueur
+     */
     public Joueur getJoueur(){
         return this.j;
     }
+
     @Override
     public String toString() {
         return intent;
     }
-
 
 }
